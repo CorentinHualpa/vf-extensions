836f7f19a9c8406479f9405d05d5c7243725e4bc
CorentinHualpa <corentin.hualpa@gmail.com>
2025-04-17 07:12:07 +0200

    Create confettis.js

---
+++ confettis.js
@@ -0,0 +1,18 @@
+export const ConfettiExtension = {
+  name: 'Confetti',
+  type: 'effect',
+  match: ({ trace }) =>
+    trace.type === 'ext_confetti' || trace.payload?.name === 'ext_confetti',
+  effect: ({ trace }) => {
+    const canvas = document.querySelector('#confetti-canvas')
+
+    var myConfetti = confetti.create(canvas, {
+      resize: true,
+      useWorker: true,
+    })
+    myConfetti({
+      particleCount: 200,
+      spread: 160,
+    })
+  },
+}

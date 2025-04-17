fbcaa2be1423517a87d28b27e59750d7bc8201c5
CorentinHualpa <corentin.hualpa@gmail.com>
2025-04-17 08:17:56 +0200

    Update confettis.js

--- confettis.js
+++ confettis.js
@@ -1,18 +1,26 @@
+// confettis.js
 export const ConfettiExtension = {
   name: 'Confetti',
   type: 'effect',
   match: ({ trace }) =>
     trace.type === 'ext_confetti' || trace.payload?.name === 'ext_confetti',
-  effect: ({ trace }) => {
-    const canvas = document.querySelector('#confetti-canvas')
-
-    var myConfetti = confetti.create(canvas, {
+  effect: () => {
+    const canvas = document.querySelector('#confetti-canvas');
+    const myConfetti = confetti.create(canvas, {
       resize: true,
       useWorker: true,
-    })
+    });
+
     myConfetti({
       particleCount: 200,
-      spread: 160,
-    })
+      spread:        160,
+      colors: [
+        '#a864fd', // violet
+        '#29cdff', // bleu
+        '#78ff44', // vert
+        '#ff718d', // rose
+        '#fdff6a'  // jaune
+      ],
+    });
   },
-}
+};
